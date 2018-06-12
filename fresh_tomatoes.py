9e469fbb434d2aa10d7c56fe19152b62a533b596
Noah Brenner <noah@noahbrenner.com>
2018-06-12 15:30:01 -0700

    Use YouTube ID provided by Movie class
    This also eliminates the use of regular expressions, making the code
    easier to read and likely improving performance.

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,6 +1,5 @@
 import webbrowser
 import os
-import re
 
 
 # The main page layout and title bar
@@ -9,9 +8,9 @@ with open('template.html', 'r') as f:
 
 # A single movie entry html template
 movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img alt="" src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{movie.youtube_id}" data-toggle="modal" data-target="#trailer">
+    <img alt="" src="{movie.poster_image_url}" width="220" height="342">
+    <h2>{movie.title}</h2>
 </div>
 '''
 
@@ -20,20 +19,8 @@ def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
-        # Extract the youtube ID from the url
-        youtube_id_match = re.search(
-            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
-                              else None)
-
         # Append the tile for the movie with its content filled in
-        content += movie_tile_content.format(
-            movie_title=movie.title,
-            poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
-        )
+        content += movie_tile_content.format(movie=movie)
     return content
 
 
